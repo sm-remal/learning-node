5b916a55869582d2a4091eb619104b637060515e
sm-remal <remal.mozzem@gmail.com>
2026-05-15 21:48:13 +0600

    update

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,12 @@ const server: Server = createServer((req: IncomingMessage, res: ServerResponse) 
 
         res.writeHead(200, { "content-type": "application/json" });
         res.end(JSON.stringify({ message: "This is root route" }));
-    } else {
+    } 
+    else if(url?.startsWith("/products")){
+        res.writeHead(200, { "content-type": "application/json" });
+        res.end(JSON.stringify({ message: "This is products route" }));
+    } 
+    else {
         res.writeHead(404, { "content-type": "application/json" });
         res.end(JSON.stringify({ message: "Route not Found !!" }));
     }
