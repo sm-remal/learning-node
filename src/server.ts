da864bdc4e3b7012a257e0f54d462777082eb69f
sm-remal <remal.mozzem@gmail.com>
2026-05-15 21:30:51 +0600

    Request & Response

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,21 @@
-import { createServer, IncomingMessage, Server } from "http";
+import { createServer, IncomingMessage, Server, ServerResponse } from "http";
 
-const server: Server = createServer((req: IncomingMessage, res) => {
-    console.log(req)
+const server: Server = createServer((req: IncomingMessage, res: ServerResponse) => {
+    // console.log(req.url)
+    // console.log(req.method)
+
+    const url = req.url;
+    const method = req.method;
+
+    if (url === "/" && method === "GET") {
+        // console.log("This is root route");
+
+        res.writeHead(200, { "content-type": "application/json" });
+        res.end(JSON.stringify({ message: "This is root route" }));
+    } else {
+        res.writeHead(404, { "content-type": "application/json" });
+        res.end(JSON.stringify({ message: "Route not Found !!" }));
+    }
 })
 
 server.listen(5000, () => {
