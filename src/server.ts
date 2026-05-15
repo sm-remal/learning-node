3322af90ebae5b53d3fb6c32adda4770fe0ee724
sm-remal <remal.mozzem@gmail.com>
2026-05-15 15:15:53 +0600

    :Server setup

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import { createServer, IncomingMessage, Server } from "http";
+
+const server: Server = createServer((req: IncomingMessage, res) => {
+    console.log(req)
+})
+
+server.listen(5000, () => {
+    console.log("Server in running on the port: 5000");
+})
+
